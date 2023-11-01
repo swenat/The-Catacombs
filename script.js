7f8351509379b5d24820ee5eaee56eeef609a8c0
Nathalie <lysefeldt@outlook.com>
2023-11-01 10:39:59 +0100

    added js plus functions startgame textnode option

---
+++ script.js
@@ -0,0 +1,14 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-btns')
+
+function startGame() {
+
+}
+
+function showTextNode(textNodeIndex) {
+
+}
+
+function selectOption(option) {
+
+}
