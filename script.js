700318a7280ec18787bad9b450cfcca7d5babbbe
Nathalie <lysefeldt@outlook.com>
2023-11-08 13:57:07 +0100

    changed images scene 2 added scenes

--- script.js
+++ script.js
@@ -48,15 +48,15 @@ function selectOption(option) { //så att vi vet vilket val som tagits
 }
 
 const textNodes = [
-    {
-      id: 1, 
+    { // First scene, entrance
+      id: 1,
       text: 'The time has come for your dream or nightmare to begin.' + '\r\n' + 'Your trip has taken you to the Catacombs of Paris.' + '\r\n' + 'The entrance reads: "Stop! This is the Empire of the dead."' + '\r\n' + 'Your guide wants to give you a map. Do you take it?',
       image: "Images/entrance_catacombs.jpg",
       options: [
-        { //valmöjligheterna
+        { //The different choices shown as buttons
             text:'Take the map',
-            setState: { mapofDoom: true }, //om karaktären väljer att ta asken
-            nextText: 2  //dit den går vidare om den tar asken
+            setState: { mapofDoom: true }, //If the caracter decides ot take the item
+            nextText: 2  //Where it goes next if the caracter has taken the item
         },
         {
             text:'Do not take the map',
@@ -64,32 +64,123 @@ const textNodes = [
         }
       ]   
     },
-    {   //Scen 2
+    {   //Scene 2 - The sign
         id: 2,
-        text: 'You proceed and enter to the first chamber. Here you meet a  dark figure, a stranger who wants to give you a note but only in exchange for the map. Beside him  on the floor you see an axe.',
-        image: "Images/catacomb1.jpg",
+        text: 'You proceed and enter to the first chamber. It leeds to a sign that you wish to read.',
+        image: "Images/catacombs_room2.png",
         options: [
             {
-                text: 'You give him your map and take the note',
-                requiredState: (currentState) => currentState.mapofDoom,  //en kontrollfunktion för att se om vi har lappen för dolda vägen
-                setState: { mapofDoom: false, secretNote: true },
+                text: 'Aproach the sign',
                 nextText: 3
+            }
+        ]   
+    },
+    {   //Scene 3 - sign approached
+        id: 3,
+        text: 'On the sign it says something in french. You do not understand what it says.',
+        image: "Images/catacomb_room2_sign.jpg",
+        options: [
+            {
+                text: 'Use the back of the map for translation',
+                requiredState: (currentState) => currentState.mapofDoom,  //Control to see if we have the map before getting the translation
+                nextText: 4
             },
             {
-                text: 'You take his axe and start running into the next room but loose the map',
-                requiredState: (currentState) => currentState.mapofDoom,  //en kontrollfunktion för att se om vi har kartan
-                setState: { mapofDoom: false, silverAxe: true },
-                nextText: 4 
+                text: 'Proceed without translation',
+                nextText: 7
+            }
+        ]   
+        
+    },
+    {   //Scene 4  - Translation of sign
+        id: 4,
+        text: 'The sign says: "Bones from the old Magdaleine cemetery deposited in 1844 in the western ossuary and transferred to the catacombs in September 1859". Interesting facts you thing to yourself while turning around to proceed.'+ '\r\n' + 'Suddenly a stranger aproaches you and wants to give you a note.',
+        image: "Images/stranger.jpg",
+        options: [
+            {
+                text: 'You hesitate first but take it',
+                setState: { secretNote: true }, 
+                nextText: 5
             },
             {
-                text: 'Ignore the stranger but keep a lookout',
-                nextText: 5 
+                text: 'You ignore the stranger',
+                requiredState: (currentState) => currentState.mapofDoom,  //en kontrollfunktion för att se om vi har lappen för dolda vägen
+                nextText: 6
+            }
+        ]        
+    },
+    {   //Scene 4 - 
+        id: 4,
+        text: 'On the note you see a riddle. If you know the answer make the right choice next. You turn around in the  new room to see if you see something. On the floor there is a knife and a torch.',
+        image: "Images/paper.png",
+        options: [
+            {
+                text: 'You take the knife',
+                requiredState: (currentState) => currentState.secretNote,
+                setState: { secretNote: false, knife: true},
+                nextText: 4
+            },
+            {
+                text: 'You take the torch',
+                setState: { secretNote: false, fireTorch: true},
+                nextText: 5
+            },
+            {
+                text: 'You take none',
+                setState: { secretNote: false},
+                nextText: 6
             }
         ]
     },
     {   //Scen 3
-        id:3,
+        id: 3,
         text: 'On the note you see a riddle. If you know the answer make the right choice next. You turn around in the  new room to see if you see something. On the floor there is a knife and a torch.',
+        image: "Images/paper.png",
+        options: [
+            {
+                text: 'You take the knife',
+                requiredState: (currentState) => currentState.secretNote,
+                setState: { secretNote: false, knife: true},
+                nextText: 4
+            },
+            {
+                text: 'You take the torch',
+                setState: { secretNote: false, fireTorch: true},
+                nextText: 5
+            },
+            {
+                text: 'You take none',
+                setState: { secretNote: false},
+                nextText: 6
+            }
+        ]
+    },
+    {   //Scen 3
+        id: 4,
+        text: 'On the note you see a riddle. If you know the answer make the right choice next. You turn around in the  new room to see if you see something. On the floor there is a knife and a torch.',
+        image: "Images/paper.png",
+        options: [
+            {
+                text: 'You take the knife',
+                requiredState: (currentState) => currentState.secretNote,
+                setState: { secretNote: false, knife: true},
+                nextText: 4
+            },
+            {
+                text: 'You take the torch',
+                setState: { secretNote: false, fireTorch: true},
+                nextText: 5
+            },
+            {
+                text: 'You take none',
+                setState: { secretNote: false},
+                nextText: 6
+            }
+        ]
+    },
+    {   //Scen 4
+        id: 4,
+        text: 'You chose the wrong item but hopefully this will help you anyway. You have entered a new room ',
         image: "Images/paper.png",
         options: [
             {
